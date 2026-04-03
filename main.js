318cfe567e6e44b24cc7ba01b8325f7c621dc71c
wizard-hash <aronidengeno@gmail.com>
2026-04-03 09:50:56 +0300

    Fadher: pip

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 const sidebar = document.getElementById('sidebar');
 const toggleBtn = document.getElementById('toggle-btn');
 
-
-toggleBtn.addEventListener('click', () => {  sidebar.classList.toggle('show');});
+if (sidebar && toggleBtn) {
+	toggleBtn.addEventListener('click', () => {
+		sidebar.classList.toggle('show');
+	});
+}
 
 
 
